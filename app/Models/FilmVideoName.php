3508f0fa283afcc4398cad9b2c15c37891111729
Meleanschi Mihail <colslaider@gmail.com>
2021-12-14 02:36:02 +0200

    Edit Film Video Model

--- app/Models/FilmVideoName.php
+++ app/Models/FilmVideoName.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class FilmVideoName extends Model
 {
     use HasFactory;
-    protected $fillable = array('name', 'id_film');
+    protected $fillable = array('name', 'id_films');
     protected $table='film_video_name';
 }
