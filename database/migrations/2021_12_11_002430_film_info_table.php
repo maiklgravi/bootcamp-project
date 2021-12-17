42eca82193281a4fdcf4a827480a7275eb5e4d6a
maiklgravi <64263307+maiklgravi@users.noreply.github.com>
2021-12-17 07:33:01 +0200

    Update 2021_12_11_002430_film_info_table.php

--- database/migrations/2021_12_11_002430_film_info_table.php
+++ database/migrations/2021_12_11_002430_film_info_table.php
@@ -16,11 +16,11 @@ class FilmInfoTable extends Migration
     {
         Schema::create('film_info', function (Blueprint $table) {
             $table->id();
-            $table->integer('film_id',15)->unique();
-            $table->string('lang',15);
-            $table->string('screenwriter',35);
+            $table->integer('film_id')->unique();
+            $table->string('lang',50);
+            $table->string('screenwriter',50);
             $table->string('actors',50);
-            $table->string('country',20);
+            $table->string('country',50);
             $table->integer('duration');
             $table->foreign('film_id')->references('id')->on('films')->cascadeOnUpdate()->cascadeOnDelete();
         });
