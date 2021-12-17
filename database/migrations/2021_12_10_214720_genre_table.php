c0101edaff8249c530c0f6b57b49165c3fa66cbb
maiklgravi <64263307+maiklgravi@users.noreply.github.com>
2021-12-17 07:12:33 +0200

    Update 2021_12_10_214720_genre_table.php

--- database/migrations/2021_12_10_214720_genre_table.php
+++ database/migrations/2021_12_10_214720_genre_table.php
@@ -16,9 +16,10 @@ class GenreTable extends Migration
     {
         DB::statement("
         CREATE TABLE `genre`(
-        `id` int NOT NULL  AUTO_INCREMENT,
-        `name` varchar(50) NOT NULL unique,
-        PRIMARY KEY(id))ENGINE=InnoDB");
+           `id` int NOT NULL  AUTO_INCREMENT,
+           `name` varchar(50) NOT NULL unique,
+            PRIMARY KEY(id)
+            )ENGINE=InnoDB");
     }
 
     /**
