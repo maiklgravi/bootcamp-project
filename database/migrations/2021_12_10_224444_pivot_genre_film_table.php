b969755725a43d6d33608f13b6ec259010d49387
maiklgravi <64263307+maiklgravi@users.noreply.github.com>
2021-12-17 07:15:18 +0200

    Update 2021_12_10_224444_pivot_genre_film_table.php

--- database/migrations/2021_12_10_224444_pivot_genre_film_table.php
+++ database/migrations/2021_12_10_224444_pivot_genre_film_table.php
@@ -16,14 +16,11 @@ class PivotGenreFilmTable extends Migration
     {
         DB::statement("
         CREATE TABLE `genre_films`(
-        `film_id` int NOT NULL,
-        `genre_id` int NOT NULL,
-        FOREIGN KEY (film_id) REFERENCES films(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-        FOREIGN KEY (genre_id) REFERENCES genre(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE)ENGINE=InnoDB");
+           `film_id` int NOT NULL,
+           `genre_id` int NOT NULL,
+            FOREIGN KEY (film_id) REFERENCES films(id) ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (genre_id) REFERENCES genre(id) ON UPDATE CASCADE ON DELETE CASCADE
+            )ENGINE=InnoDB");
     }
 
     /**
