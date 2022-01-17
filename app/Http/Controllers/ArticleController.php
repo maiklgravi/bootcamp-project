f06edccde217dc30f9e841e14ebf4e62c43f5646
Meleanschi Mihail <colslaider@gmail.com>
2022-01-17 23:10:53 +0200

    Integrate in ArticleController logging of Article read.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,12 +7,23 @@ use App\Models\Article;
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use App\Models\Comment;
+use Psr\Log\LoggerInterface;
 
 class ArticleController extends Controller
 {
-    public function show ($articlesId){
+    public function show ($articlesId, Request $request, LoggerInterface $logger){
         $article = Article::findOrFail($articlesId);
         $comments =  $article->comments()->paginate(2);
+        $user = $request->user();
+        $userReprezentation = $user ? "User with id {$user->id}" : "Unknown User";
+        $logger->info(
+            $userReprezentation . " accesed " . "article with id{$articlesId}", 
+            ['id' => $articlesId ,
+             'title'=> $article->title,
+             'author_id'=>$article->author_id,
+             'seo_title'=>$article->seo_title,
+             'published_at'=>$article->published_at,
+            ]);
         return view('article.article' , [
             'article' => $article , 
             'comments' => $comments , 
