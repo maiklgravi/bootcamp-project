c37f13458df74e8299ff9338954aeda46731b2c3
Meleanschi Mihail <colslaider@gmail.com>
2022-01-20 21:41:44 +0200

    add url in return for prodaction request activity logger

--- app/Services/ProductionRequestActivityLogger.php
+++ app/Services/ProductionRequestActivityLogger.php
@@ -9,6 +9,7 @@ class ProductionRequestActivityLogger extends AbstractRequestActivityLogger
     protected function collectRequestData(Request $request): array
     {
         return [
+        'url'=>$request->path(),
         'ipAddress'=>$request->ip(),
         ];
     }
