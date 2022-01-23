a875988c92bd05618a8d4aeb82f4fd0f0bd97a6d
Meleanschi Mihail <colslaider@gmail.com>
2022-01-23 20:23:58 +0200

    Add two command first to play Russion Roullete , two to show result.

---
+++ app/Console/Commands/RussionRouletteCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+
+class RussionRouletteCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'game:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'RussionRoulette game were youser win or lose.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(CacheRepository $cacheRepository)
+    {
+        parent::__construct();
+        $this->cacheRepository = $cacheRepository;
+        
+    }
+ 
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        
+        $user = $this->ask("What you user id?");
+        $win = $this->cacheRepository->get('WinStatistic', []);
+        $lose = $this->cacheRepository->get('loseStatistic', []);
+        $win[$user] = $win[$user] ?? 0;
+        $lose[$user] = $lose[$user] ?? 0;
+        $random = random_int(0,1);
+        if ($random == 1) {
+        $this->info("you win");
+        $win[$user]++;  
+        }else {    
+            $this->info("you lose");
+        $lose[$user]++;   
+        };
+        
+        $this->cacheRepository->set('WinStatistic', $win);
+        $this->cacheRepository->set('loseStatistic', $lose);
+               
+        
+    }
+}
---
+++ app/Console/Commands/ShowStatusCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+
+class ShowStatusCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'status:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(CacheRepository $cacheRepository, ConfigRepository $configRepository)
+    {
+        parent::__construct();
+        $this->cacheRepository = $cacheRepository;
+        
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $win = $this->cacheRepository->get('WinStatistic', []);
+        $lose = $this->cacheRepository->get('loseStatistic', []);
+        $table = [];
+
+
+        foreach ($win as $key => $count ) {
+            $table[] = [$key, $count ];
+        }
+
+        $this->table(['user', 'win' ], $table);
+        $table = [];
+        foreach ($lose as $key => $count ) {
+            $table[] = [$key, $count ];
+        }
+    
+        $this->table(['user', 'lose' ], $table);
+        $this->cacheRepository->set('WinStatistic', $win);
+        $this->cacheRepository->set('loseStatistic', $lose);
+               
+    }
+}
